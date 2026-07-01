0c77bf59656da2325b4f34d187690953ed7c1490
Sanghoon Lee <shoon114@icloud.com>
2026-07-01 12:45:49 +0900

    [refactoring] Page.20
    Signed-off-by: Sanghoon Lee <shoon114@icloud.com>

--- baseball.cpp
+++ baseball.cpp
@@ -6,6 +6,8 @@ using std::string;
 class Baseball {
 public:
 	void guess(const string& string) {
-		throw std::length_error("Must be three letters.");
+		if (string.length() != 3) {
+			throw std::length_error("Must be three letters.");
+		}
 	}
 };
