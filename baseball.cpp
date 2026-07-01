6675f251a67c209a7aafdbd08a5b59290096348a
Sanghoon Lee <shoon114@icloud.com>
2026-07-01 12:58:17 +0900

    [refactoring] Page.34
    Signed-off-by: Sanghoon Lee <shoon114@icloud.com>

--- baseball.cpp
+++ baseball.cpp
@@ -6,20 +6,29 @@ using std::string;
 class Baseball {
 public:
 	void guess(const string& guessNumber) {
+		assertIllegalArgument(guessNumber);
+	}
+
+	void assertIllegalArgument(const std::string& guessNumber)
+	{
 		if (guessNumber.length() != 3) {
 			throw std::length_error("Must be three letters.");
 		}
 
 		for (char ch : guessNumber) {
-			if (ch < '0' || ch > '9') {
-				throw std::invalid_argument("Must be number");
-			}
+			if (ch >= '0' && ch <= '9') continue;
+			throw std::invalid_argument("Must be number");
 		}
 
-		if (guessNumber[0] == guessNumber[1]
+		if (isDuplicateNumber(guessNumber)) {
+			throw std::invalid_argument("Must not have the same number");
+		}
+	}
+
+	bool isDuplicateNumber(const std::string& guessNumber)
+	{
+		return guessNumber[0] == guessNumber[1]
 			|| guessNumber[0] == guessNumber[2]
-			|| guessNumber[1] == guessNumber[2]) {
-				throw std::invalid_argument("Must not have the same number");
-			}
+			|| guessNumber[1] == guessNumber[2];
 	}
 };
