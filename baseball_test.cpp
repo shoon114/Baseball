710b546e3de122fe77ed77fa354bab4ea93518ce
Sanghoon Lee <shoon114@icloud.com>
2026-07-01 12:36:22 +0900

    초기 커밋.

---
+++ baseball_test.cpp
@@ -0,0 +1,4 @@
+#include "baseball.cpp"
+#include "gmock/gmock.h"
+
+using namespace testing;
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include "gmock/gmock.h"
+using namespace testing;
+
+int main() {
+	InitGoogleMock();
+	return RUN_ALL_TESTS();
+}
