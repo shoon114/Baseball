e96b5988084a95feadede86257e4c9ba3e468a22
Sanghoon Lee <shoon114@icloud.com>
2026-07-01 13:05:43 +0900

    [reature] Page.43
    Signed-off-by: Sanghoon Lee <shoon114@icloud.com>

--- baseball.cpp
+++ baseball.cpp
@@ -3,10 +3,24 @@
 
 using std::string;
 
+struct GuessResult {
+	bool solved;
+	int strikes;
+	int balls;
+};
+
 class Baseball {
 public:
-	void guess(const string& guessNumber) {
+	explicit Baseball(const string& question) : question(question) {
+
+	}
+
+	GuessResult guess(const string& guessNumber) {
 		assertIllegalArgument(guessNumber);
+		if (guessNumber == question) {
+			return { true, 3, 0 };
+		}
+		return { false, 0, 0 };
 	}
 
 	void assertIllegalArgument(const std::string& guessNumber)
@@ -31,4 +45,7 @@ public:
 			|| guessNumber[0] == guessNumber[2]
 			|| guessNumber[1] == guessNumber[2];
 	}
+
+private:
+	string question;
 };
--- baseball_test.cpp
+++ baseball_test.cpp
@@ -7,7 +7,7 @@ using std::exception;
 
 class BaseballFixture : public Test {
 public:
-	Baseball game;
+	Baseball game{ "123" };
 	void assertIllegalArgument(string guessNumber) {
 		try {
 			game.guess(guessNumber);
@@ -23,4 +23,12 @@ TEST_F(BaseballFixture, ThrowExceptionWhenInvalidCase) {
 	assertIllegalArgument("12");
 	assertIllegalArgument("12s");
 	assertIllegalArgument("121");
+}
+
+TEST_F(BaseballFixture, ReturnSolveResultIfMatchedNumber) {
+	GuessResult result = game.guess("123");
+
+	EXPECT_TRUE(result.solved);
+	EXPECT_EQ(3, result.strikes);
+	EXPECT_EQ(0, result.balls);
 }
